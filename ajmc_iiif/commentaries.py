0d5eb4762a9b77efeff4d2715d87cf24aff7662b
Charles Pletcher <pletcher@users.noreply.github.com>
2023-10-09 16:26:50 +0200

    Add images (big push!)

--- ajmc_iiif/commentaries.py
+++ ajmc_iiif/commentaries.py
@@ -5,6 +5,8 @@ import ajmc_iiif.image as image
 import os
 import pathlib
 
+from tqdm import tqdm
+
 
 COMMENTARIES_METADATA_SHEET_ID = "1jaSSOF8BWij0seAAgNeGe3Gtofvg9nIp_vPaSj5FtjE"
 COMMENTARIES_METADATA_SHEET_NAME = "bibliographic_metadata"
@@ -23,11 +25,10 @@ class Commentary:
         self.pngs_dir = base_directory / self.id / "images" / "png"
 
     def create_derivatives(self):
-        for png in self.pngs_dir.iterdir():
+        for png in tqdm(self.pngs_dir.iterdir(), desc=self.id):
             if png.suffix == ".png":
                 full_size = image.FullSize(self.id, png)
                 thumbnail = image.Thumbnail(self.id, png)
-
                 info = iiif.Info(self.id, png.stem, [full_size, thumbnail])
                 info.write()
 
@@ -43,6 +44,6 @@ class PublicDomainCommentaries:
         )
 
     def create_derivatives(self):
-        for commentary_id in self.commentary_ids:
+        for commentary_id in tqdm(self.commentary_ids, desc="all commentaries"):
             commentary = Commentary(self._base_directory, commentary_id)
             commentary.create_derivatives()
