8928afd318a90876e9cbfdcc86071a523006a051
Matteo Romanello <matteo.romanello@unil.ch>
2025-03-05 11:15:13 +0100

    unpinned ajmc dependency from requirements.txt as this was producing a not-up-to-date list of public domain commentary IDs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import ajmc_iiif.commentaries as commentaries
 
 def main():
     public = commentaries.prepare_commentaries(public_domain=True)
+    print(public.commentary_ids)
     public.create_derivatives()
 
 
